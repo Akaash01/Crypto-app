57658bc0a3afb8150954bdfeba6f233badf6daff
Akaash01 <54339987+Akaash01@users.noreply.github.com>
2022-07-23 18:06:31 +0530

    chart added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,8 +14,19 @@ export const cryptoApi = createApi({
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`)
+    }),
+    getCryptosDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`)
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`/coin/${coinId}/history?timePeriod=${timePeriod}`)
     })
   })
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const {
+  useGetCryptosQuery,
+  useGetCryptosDetailsQuery,
+  useGetCryptoHistoryQuery
+} = cryptoApi;
